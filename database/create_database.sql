5a0669bacf279a8c4d6bf1583e699373e15104f4
torstein-vik <torsteinv64@gmail.com>
2017-06-06 13:43:31 +0200

    Created database

--- database/create_database.sql
+++ database/create_database.sql
@@ -16,8 +16,45 @@ CREATE TABLE IF NOT EXISTS `users` (
   `passhash` BINARY(32) NOT NULL,
   `passsalt` BINARY(32) NOT NULL,
   `privilege` ENUM('user', 'admin') NOT NULL,
+  `address` VARCHAR(45) NOT NULL,
+  `postnummer` VARCHAR(4) NOT NULL,
+  `telefonnummer` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`userid`),
+  FOREIGN KEY (`postnummer`)
+    REFERENCES `poststed` (`postnummer`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+ENGINE = InnoDB;
+
+-- Apartment table
+CREATE TABLE IF NOT EXISTS `apartments` (
+  `apartmentid` INT NOT NULL AUTO_INCREMENT,
+  `apartmentnumber` CHAR(4) NOT NULL,
+  PRIMARY KEY (`apartmentid`))
+ENGINE = InnoDB;
+
+-- Apartment image table
+CREATE TABLE IF NOT EXISTS `apartment_images` (
+  `apartment_imageid` INT NOT NULL AUTO_INCREMENT,
+  `apartmentid` INT NOT NULL,
+  `path` VARCHAR(45) NOT NULL,
+  `imagetext` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`apartment_imageid`),
+  FOREIGN KEY (`apartmentid`)
+    REFERENCES `apartments` (`apartmentid`))
+ENGINE = InnoDB;
+
+-- Reservations table
+CREATE TABLE `reservations` (
+  `reservationid` INT NOT NULL AUTO_INCREMENT,
+  `userid` INT NOT NULL,
+  `apartmentid` INT NOT NULL,
+  `date_start` TIMESTAMP NOT NULL,
+  `date_end` TIMESTAMP NOT NULL,
+  PRIMARY KEY(`reservationid`),
+  FOREIGN KEY (`userid`)
+    REFERENCES `users` (`userid`),
+  FOREIGN KEY (`apartmentid`)
+    REFERENCES `apartments` (`apartmentid`))
 ENGINE = InnoDB;
 
 -- Poststed table
