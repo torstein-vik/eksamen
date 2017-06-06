a57973c0d119dd8870c11a96f1f5306a62b8f424
Torstein Vik <torsteinv64@gmail.com>
2017-06-07 00:33:56 +0200

    Renewed content

--- database/content.sql
+++ database/content.sql
@@ -19,21 +19,21 @@ ALTER TABLE `apartment_images` AUTO_INCREMENT = 1;
 
 
 INSERT INTO apartments (apartmentnumber, description, price, featured_img) VALUES
-('101', 'Fantastisk utsikt over fjorden', '100', '1'),
-('202', 'Fantastisk utsikt over fjorden 2', '200', '4'),
-('303', 'Fantastisk utsikt over fjorden 3', '300', '6'),
-('404', 'Fantastisk utsikt over fjorden 4', '400', '8');
+('101', '2-etasjers leilighet med kjøkken, 2 bad, stue, og 2 soverom. Vi kan love fantastisk utsikt over fjorden!', '499', '1'),
+('202', 'Koslig flat leilighet med stor stue, 1 bad, og ett soverom. Hva skal med kjøkken, sant?', '199', '4'),
+('303', 'Denne store leiligheten har alt man trenger; 5 bad, 3 soverom, og kjempestor stue. I tillegg har den et skikkelig kjøkken!', '999', '7'),
+('404', 'Rimelig og koslig leilighet med stue, bad, og soverom. ', '99', '8');
 
 
 INSERT INTO apartment_images (apartmentid, path, imagetext) VALUES
-('1', 'res/placeholder.png', 'Placeholder 123'),
-('1', 'res/placeholder.png', 'Placeholder 321'),
-('2', 'res/placeholder.png', 'Placeholder 1234'),
-('2', 'res/placeholder.png', 'Placeholder 4321'),
-('3', 'res/placeholder.png', 'Placeholder 12345'),
-('3', 'res/placeholder.png', 'Placeholder 54321'),
-('4', 'res/placeholder.png', 'Placeholder 123456'),
-('4', 'res/placeholder.png', 'Placeholder 654321');
+('1', 'res/apartment1a.jpg', 'Koslig kjøkken'),
+('1', 'res/apartment1b.jpg', 'Moderne stue'),
+('2', 'res/apartment2a.jpg', 'Kvit sofa'),
+('2', 'res/apartment2b.jpg', 'hvitt bord med plante på'),
+('3', 'res/apartment3a.jpg', 'Luksuriøst kjøkken'),
+('3', 'res/apartment3b.jpg', 'Dekt bord med utsikt'),
+('3', 'res/apartment3b.jpg', 'Den store stua'),
+('4', 'res/apartment4a.jpg', 'Den rimelige stua');
 
 
 -- Reenable foreign key checks
